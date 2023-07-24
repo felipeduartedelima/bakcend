39d0be1e7654ac355ab3a5a6534f05101c4e8163
Felipe Duarte de Lima <duartedelimafelipe@gmail.com>
2023-07-24 00:04:21 -0300

    chore: tEST COMMANDS AND CI/CD IT CAN BE REMOVE AFTER

---
+++ src/file.spec.ts
@@ -0,0 +1,7 @@
+import { fn } from "./file"
+
+describe('test', () => {
+  it('test:2', () => {
+    expect(fn(12)).toEqual(false)
+  })
+})
---
+++ src/file.test.ts
@@ -0,0 +1,7 @@
+import { fn } from "./file"
+
+describe('test', () => {
+  it('test:3', () => {
+    expect(fn(20)).toEqual(true)
+  })
+})
